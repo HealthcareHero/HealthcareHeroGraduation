7427ff9ef4b4d58ebb251906e09e75ffc7ff79e5
marcusngwj <marcusngwj@msn.com>
2020-05-05 23:37:03 +0800

    update text for hero bernard

--- client/src/components/profile/heroes/bernard/index.js
+++ client/src/components/profile/heroes/bernard/index.js
@@ -16,9 +16,10 @@ function Bernard() {
           A True Calling
         </div>
         <div>
-          Everyone hopes to have good health to pursue what they wish in life.
-          For Bernard, he took a step further by facilitating the process of bringing others back on the road of recovery.
           Anyone who had their health compromised would understand the inconvenience and hardship.
+          Everyone hopes to have good health to pursue their dreams in life.
+          For Bernard, it does not stop there.
+          Having the desire to care, serve and help, he takes a step further by facilitating the process of bringing others back on their road of recovery.
         </div>
       </div>
       
@@ -27,9 +28,9 @@ function Bernard() {
           Fighting Stereotype
         </div>
         <div>
-          Nursing has evolved overtime from just a stereotypical bed side care.
-          Today, nurses are equipped with more automony and knowledge to function.
-          This gives new nurses more choices to branch out into variety of job scopes within the nursing field.
+          Nursing has evolved from just a stereotypical bedside care.
+          Today, nurses are equipped with more autonomy and knowledge to function.
+          This gives new nurses more choices to branch out into a variety of job scopes within the nursing field.
           Generally, the starting point for most nurses who enter by choice stems from the act of providing face-to-face care through human interactions.
           Nurses can be considered to be the one with the highest element of personal touch.
         </div>
@@ -41,24 +42,24 @@ function Bernard() {
         </div>
         <img src={involvementImg} alt="involvement" style={mediaSectionImage} />
         <div style={mediaSectionText}>
-          Basic academic studies, projects and skill training are nothing out of the ordinary.
-          Community work is what interest Bernard the most.
-          Bernard belives that life in nursing academy is only as fruitful as what one intends it to be.
-          The more involved he chose to be, the more he sees in terms of people who are struggling from various walks of life.
-          It can be an eye-opening experience compared to just hearing news or seeing articles online, which can never substitute real life experience. 
+          Basic academic studies, projects and skill trainings are nothing out of the ordinary.
+          Community work is what interests Bernard the most.
+          Bernard believes that life in the nursing academy is only as fruitful as what one intends it to be.
+          The more involved he chose to be, the more he understands people from various walks of life.
+          It is an eye-opening experience compared to just hearing news or seeing articles online, which can never substitute real-life experience. 
         </div>
       </div>
 
       <div style={section}>
         <div style={sectionHeader}>
-          Education is primary
+          Learning through experience
         </div>
         <div>
           Tutorials and lectures do not spark joy for a kinaesthetic learner like Bernard.
           Instead, most of the lessons learnt are from T2P.
-          Technically for most people it would be their virgin experience of handling patients as in-charge.
-          Naturally, it would be stressful as now people who have come to seek medical attention are under your responsibility and care. 
-          One's decisions along with other nursing colleagues would affect the health outcomes of the patients during their hospitalization period.
+          Technically for most people, it would be their virgin experience of handling patients as in-charge.
+          Naturally, it would be stressful as people who have come to seek medical attention are under your responsibility and care. 
+          One's decisions, along with other nursing colleagues, would affect the health outcomes of the patients during their hospitalization period.
           It is a huge responsibility.
         </div>
       </div>
@@ -68,17 +69,19 @@ function Bernard() {
           To all aspiring nurses
         </div>
         <div>
-          Even before choosing this course, do your own research.
-          Ask around people who are in the field, search for relevant friends to seek advice.
-          Attend talks, seminar, open house etc. 
+          Bernard would like to offer some advice to prospective nurses.
+          <br /><br />
+          Before choosing this course, do your research.
+          Seek advice and consult others on this profession.
+          Attend talks, seminars, open house etc. 
           Apply for attachments, job shadowing, clinically jobs or anything that broadens your perspective of this field before deciding whether it is something you may consider doing. 
           <br /><br />
           After choosing the course, always humble yourself throughout the learning process.
-          It’s good to curious and seek understanding, just not be rude.
-          Learn to take criticism, deal with negative remarks/comments from people who still have prejudice against nurses. Society is a tough bunch to handle, welcome to adult world. 
+          It’s good to be curious and clarify doubts, but be tactful.
+          Learn to take criticism, deal with negative remarks/comments from others, but do not take it personally.
           <br /><br />
-          Lastly, find and bond with people who can be there for you on a emotional level when needed.
-          Try and get along with people, it does not hurt.
+          Lastly, find and bond with people who can be there for you on an emotional level when needed.
+          Do get along with others, it does not hurt.
         </div>
       </div>
 

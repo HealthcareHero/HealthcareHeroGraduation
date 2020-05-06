80949e325d19d3013afb35a3efbda66c1d9f72ed
marcusngwj <marcusngwj@msn.com>
2020-05-06 23:27:58 +0800

    relocate image

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -4,19 +4,19 @@ export const profile = [
   {
     name: "Bernard",
     path: path.profile.bernard,
-    img: require('../../assets/img/avatar_bernard.png'),
+    img: require('../../assets/img/profile-avatar/avatar_bernard.png'),
     component: require('./heroes/bernard').default
   },
   {
     name: "Yanka",
     path: path.profile.yanka,
-    img: require('../../assets/img/avatar_yanka.png'),
+    img: require('../../assets/img/profile-avatar/avatar_yanka.png'),
     component: require('./heroes/yanka').default
   },
   {
     name: "Yik Siong",
     path: path.profile.yiksiong,
-    img: require('../../assets/img/avatar_yiksiong.png'),
+    img: require('../../assets/img/profile-avatar/avatar_yiksiong.png'),
     component: require('./heroes/yik-siong').default
   }
 ];

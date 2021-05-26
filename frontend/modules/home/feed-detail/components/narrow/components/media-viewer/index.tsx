6540a194f198fb21db34cf254da0dfde9da9314c
Marcus Ng <marcusngwj@msn.com>
2021-05-26 21:05:24 +0800

    fix media viewer style

--- frontend/modules/home/feed-detail/components/narrow/components/media-viewer/index.tsx
+++ frontend/modules/home/feed-detail/components/narrow/components/media-viewer/index.tsx
@@ -1,7 +1,6 @@
-import { styles } from './styles/index.style'
 import { MediaViewerProps } from './types/index.type'
 import MediaVisualViewer from 'common/components/media-visual-viewer'
 
 export default function MediaViewer({ media }: MediaViewerProps) {
-  return <MediaVisualViewer media={media} styles={styles} />  
+  return <MediaVisualViewer media={media} />  
 }
--- frontend/modules/home/feed-detail/components/narrow/components/media-viewer/styles/index.type.ts
+++
@@ -1,4 +0,0 @@
-export interface Css {
-  container: string;
-  img: string;
-}

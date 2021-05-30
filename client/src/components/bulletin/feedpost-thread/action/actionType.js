d798849de276294f81b32ef4bfd41cedda58923c
Marcus Ng <marcusngwj@msn.com>
2021-05-30 19:27:08 +0800

    test deploy vercel

--- client/src/components/bulletin/feedpost-thread/action/actionType.js
+++
@@ -1,8 +0,0 @@
-export const actionType = {
-  initialiseFeedPostThread: "INITIALISE_FEED_POST_THREAD",
-  loadingFeedPostThread: "LOADING_FEED_POST_THREAD",
-  errorWhileLoading: "ERROR_WHILE_LOADING_FEED_POST_THREAD",
-  updateLikeCount: "UPDATE_LIKE_COUNT",
-  updateInputText: "UPDATE_INPUT_TEXT",
-  submittingComment: "SUBMITTING_COMMENT"
-}

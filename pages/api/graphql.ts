1e2bdb5f566dec7c3cdc56adce1cef9e5df64fa4
Marcus Ng <marcusngwj@msn.com>
2021-06-03 23:07:11 +0800

    Try fix issue by changing back url

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,8 +1,7 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { mergedSchema } from 'server'
 
-// const API_PATH = "http://localhost:3000/api/graphql";
-const API_PATH = "https://healthcare-hero-graduation-9e7ayjnx9-healthcarehero.vercel.app/api/graphql";
+const API_PATH = "/api/graphql";
 
 const apolloServer = new ApolloServer({ schema: mergedSchema, playground: true, debug: true });
 

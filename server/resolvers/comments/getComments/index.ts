e5a9c5bb27c0e7579d12113ecf8a113c53d0880d
Marcus Ng <marcusngwj@msn.com>
2021-06-20 22:59:00 +0800

    fix build issue - regression

--- server/resolvers/comments/getComments/index.ts
+++ server/resolvers/comments/getComments/index.ts
@@ -1,17 +1,12 @@
-import { GetCommentsArgument } from './index.type'
-import { Comment } from 'server/models/comments/index.type'
+import { GetCommentsParent, GetCommentsRequest, GetCommentsResult } from './index.type'
 import { execute } from 'server/data-access/execute'
 import { getComments as service} from 'server/data-access/execute/services/comments/getComments'
 import { mock_getComments as mockService} from 'server/data-access/execute/mocks/services/comments/getComments'
 
-export const getComments = async (parent: any, args: any): Promise<Comment[] | null> => {
-  // const { feedPostId } = args as GetCommentsArgument;
-  const { id } = parent;
-
-  console.log("PARENT", parent)
-  const feedPostId = id;
-  const request = { feedPostId }
-  const result = execute(service(request), mockService(request));
+export const getComments = async (parent: any, args: any): Promise<GetCommentsResult> => {
+  const { id } = parent as GetCommentsParent;
+  const request: GetCommentsRequest = { feedPostId: id }
+  const result = execute<GetCommentsResult>(() => mockService(request), () => mockService(request));
   console.log(result)
 
   return result;
--- server/resolvers/comments/getComments/index.type.ts
+++ server/resolvers/comments/getComments/index.type.ts
@@ -1,3 +1,9 @@
-export interface GetCommentsArgument {
-  feedPostId: string;
+import { GetCommentsRequest, GetCommentsResponse } from 'server/data-access/execute/services/comments/getComments/index.type'
+
+export type { GetCommentsRequest };
+
+export type GetCommentsResult = GetCommentsResponse;
+
+export interface GetCommentsParent {
+  id: string;
 }

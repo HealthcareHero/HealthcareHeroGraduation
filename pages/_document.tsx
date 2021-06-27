f19de676f54bc214df2a8697d8075f9a18809adc
Marcus Ng <marcusngwj@msn.com>
2021-06-27 23:56:22 +0800

    all image to load

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,10 @@ class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="
               script-src 'self' ;
-              img-src 'self' data: https://vitals.vercel-insights.com/v1/vitals https://storage.googleapis.com/nursing-class-of-2020.appspot.com/uploads/eadd6ed2-cbfd-47a5-9e67-3faa75bd7db8.png https://www.nme.com https://blog.playstation.com https://console.kr-asia.com https://i.pinimg.com blob: ;
+              img-src 'self' data: https://vitals.vercel-insights.com/v1/vitals https://storage.googleapis.com https://www.nme.com https://blog.playstation.com https://console.kr-asia.com https://i.pinimg.com blob: ;
               style-src 'self' 'unsafe-inline' ;
               font-src 'self' ;
-              connect-src 'self' https://vitals.vercel-insights.com/v1/vitals https://storage.googleapis.com/nursing-class-of-2020.appspot.com ;
+              connect-src 'self' https://vitals.vercel-insights.com/v1/vitals ;
               manifest-src 'self' ; 
               prefetch-src 'self' https://vitals.vercel-insights.com/v1/vitals ;
               default-src 'self' https://vitals.vercel-insights.com/v1/vitals ; 

d6caf897af08a340f8c2e9a64daab3ba844496eb
Marcus Ng <marcusngwj@msn.com>
2021-06-30 23:49:33 +0800

    Update firebase functions

--- server/data-access/integrations/firebase/actions/index.ts
+++ server/data-access/integrations/firebase/actions/index.ts
@@ -1,14 +1,35 @@
-import { firestore } from '../../firebase'
-import { DocumentData, DocumentSnapshot, QuerySnapshot } from '@google-cloud/firestore';
+import { firestore, fieldValue } from '../../firebase'
+import { DocumentData, DocumentReference, DocumentSnapshot, QuerySnapshot, WriteResult } from '@google-cloud/firestore';
 
-export const addNewRecord = async (collection: string, item: any) => {
-  return await firestore.collection(collection).add(Object.assign({}, item));
-}
-
-export const getRecords = async (collection: string, orderByField: string, isAsc: boolean): Promise<QuerySnapshot<DocumentData>> => {
+export const getCollection = async (collection: string, orderByField: string, isAsc: boolean): Promise<QuerySnapshot<DocumentData>> => {
   return await firestore.collection(collection).orderBy(orderByField, isAsc ? 'asc' : 'desc').get()
 }
 
-export const getDocument = async (collection: string, id: string): Promise<DocumentSnapshot<DocumentData>> => {
-  return await firestore.collection(collection).doc(id).get();
+export const addDocument = async (collection: string, item: any) => {
+  return await firestore.collection(collection).add(Object.assign({}, item));
+}
+
+export const getDocument = async (collection: string, docId: string): Promise<DocumentSnapshot<DocumentData>> => {
+  return await firestore.collection(collection).doc(docId).get();
+}
+
+export const addDocumentToSubCollection = async (collection: string, docId: string, subCollection: string, item: any): Promise<DocumentReference<DocumentData>> => {
+  const document = await firestore.collection(collection).doc(docId).get();
+
+	if (document) {
+		return await firestore.collection(collection).doc(docId).collection(subCollection).add(Object.assign({}, item));
+		// return response.id;
+	}
+  else {
+    throw new Error("TODO firebase error")
+  }
+}
+
+export const getSubCollection = async (collection: string, docId: string, subCollection: string, orderByField: string, isAsc: boolean): Promise<QuerySnapshot<DocumentData>> => {
+  return await firestore.collection(collection).doc(docId).collection(subCollection).orderBy(orderByField, isAsc ? 'asc' : 'desc').get();
+}
+
+export const incrementDocumentField = async (collection: string, docId: string, field: string, amount: number): Promise<WriteResult>  => {
+  return await firestore.collection(collection).doc(docId).update({[field]: fieldValue.increment(amount)});
+  // return boolean
 }
